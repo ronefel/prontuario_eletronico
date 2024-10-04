76512f09d172f44c19c0215d10ceacbf030ea788
ronefel <rone@shifttecnologia.com.br>
2024-10-04 18:39:46 -0400

    .

--- app/Http/Controllers/ProntuarioController.php
+++ app/Http/Controllers/ProntuarioController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Prontuario;
 use App\Models\Setting;
+use App\Services\PDFGeneratorService;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Mpdf\Mpdf;
 
@@ -15,107 +17,35 @@ class ProntuarioController extends Controller
         $prontuario = Prontuario::findOrFail($id);
 
         // Recuperar os parâmetros adicionais da URL
-        $layout = $request->query('layout', 'P'); // 'portrait' é o valor padrão
+        $layout    = $request->query('layout', 'P'); // 'portrait' é o valor padrão
         $paperSize = $request->query('paper_size', 'A4'); // 'A4' é o valor padrão
 
-        // Busca todas as configurações
-        $settings = Setting::getAllSettings();
-
-        // Busca os valores de cabeçalho e rodapé do modelo Setting
-        $cabecalho = $settings[Setting::CABECALHO];
-        $rodape = $settings[Setting::RODAPE];
-
-        // Variáveis dinâmicas que serão substituídas
-        // ================================== NÃO ALTERAR ESTAS VARIÁVEIS ==================================
-        $variaveis = [
-            '{PAC_NOME}' => $prontuario->paciente->nome,
-            '{NASCIM}' => $prontuario->paciente->nascimento->format('d/m/Y'),
-            '{IDADE}' => $prontuario->paciente->idade(),
-            '{SEXO}' => $prontuario->paciente->sexo(),
-            '{TIPO}' => $prontuario->paciente->tiposanguineo,
-            '{PAC_CPF}' => $prontuario->paciente->cpf,
-            '{PAC_CELULAR}' => $prontuario->paciente->celularFormatado(),
-            '{PAC_EMAIL}' => $prontuario->paciente->email,
-            '{PAC_CEP}' => $prontuario->paciente->cep,
-            '{PAC_LOGRADOURO}' => $prontuario->paciente->logradouro,
-            '{PAC_NUMERO}' => $prontuario->paciente->numero,
-            '{PAC_BAIRRO}' => $prontuario->paciente->bairro,
-            '{PAC_COMPLEMENTO}' => $prontuario->paciente->complemento,
-            '{PAC_CIDADE}' => $prontuario->paciente->cidade?->cidadeUf(),
-
-            '{DATA_ATENDIMENTO}' => $prontuario->data->format('d/m/Y'),
-        ];
-
-
-        // Substitui os placeholders no cabecalho, descricao e rodape
-        $cabecalho = str_replace(array_keys($variaveis), array_values($variaveis), $cabecalho);
-        $prontuario->descricao = str_replace(array_keys($variaveis), array_values($variaveis), $prontuario->descricao);
-        $rodape = str_replace(array_keys($variaveis), array_values($variaveis), $rodape);
-
-        // Substitui <hr /> por <div style="border-bottom: 2px solid #000;"></div>
-        $prontuario->descricao = str_replace('<hr />', '<div style="border-bottom: 1px solid #000;"></div>', $prontuario->descricao);
-        $cabecalho = str_replace('<hr />', '<div style="border-bottom: 2px solid #000;"></div>', $cabecalho);
-        $rodape = str_replace('<hr />', '<div style="border-bottom: 2px solid #000;"></div>', $rodape);
-
-        $margemSuperior = $settings[Setting::MARGEM_SUPERIOR];
-        $margemInferior = $settings[Setting::MARGEM_INFERIOR];
-        $margemEsquerda = $settings[Setting::MARGEM_ESQUERDA];
-        $margemDireita = $settings[Setting::MARGEM_DIREITA];
-        $alturaCabecalho = $settings[Setting::ALTURA_CABECALHO];
-        $alturaRodape = $settings[Setting::ALTURA_RODAPE];
-
-        $formato = $paperSize . '-' . $layout;
-        if ($paperSize === 'A5noA4') {
-            $formato = 'A4-L';
-            $margemDireita += 148;
+        // Validação manual dos parâmetros
+        if (!in_array($layout, ['P', 'L'])) {
+            throw new HttpResponseException(response()->make(
+                'O parâmetro layout deve ser P (portrait) ou L (landscape).',
+                400
+            ));
         }
 
+        if (!in_array($paperSize, ['A4', 'A5', 'A5noA4'])) {
+            throw new HttpResponseException(response()->make(
+                'O parâmetro paper_size deve ser A4, A5 ou A5noA4.',
+                400
+            ));
+        }
 
-        // Configurando o mPDF
-        $mpdf = new Mpdf([
-            'format' => $formato,
-            'fontDir' => [public_path('fonts/Inter')],
-            'fontdata' => [
-                'inter' => [
-                    'R' => 'Inter-VariableFont_opsz,wght.ttf',  // Caminho para o arquivo de fonte regular
-                    'I' => 'Inter-Italic-VariableFont_opsz,wght.ttf', // Caminho para o arquivo de fonte itálico
-                ],
-            ],
-            'default_font' => 'inter',  // Define a fonte Inter como padrão
-            'margin_left' => $margemEsquerda,
-            'margin_right' => $margemDireita,
-            'margin_top' => $alturaCabecalho + $margemSuperior,
-            'margin_bottom' => $alturaRodape + $margemInferior,
-            'margin_header' => $margemSuperior,
-            'margin_footer' => $margemInferior,
-            'setAutoTopMargin' => 'stretch',
-            'setAutoBottomMargin' => 'stretch',
-            'autoMarginPadding' => -7,
-        ]);
+        $pdfGeneratorService = new PDFGeneratorService(
+            $paperSize,
+            $layout,
+            $prontuario->paciente,
+            $prontuario->data
+        );
 
-        // Define o cabeçalho e o rodapé do PDF
-        $mpdf->SetHTMLHeader('
-            <div class="document-content">
-                ' . $cabecalho . '
-            </div>
-        ');
-        $mpdf->SetHTMLFooter('
-            <div class="document-content">
-                ' . $rodape . '
-            </div>
-        ');
+        $mpdf = $pdfGeneratorService->generatePDF(
+            view('pdf.prontuario', ['prontuario' => $prontuario])->render(),
+        );
 
-        // Conteúdo HTML do PDF
-        $htmlContent = view('pdf.prontuario', ['prontuario' => $prontuario])->render(); // Pega o conteúdo da view 'pdf.template'
-
-        // Adiciona o CSS
-        $stylesheet = file_get_contents('vendor\ckeditor\document-content.css');
-        $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
-
-        // Passa o conteúdo HTML para o mPDF
-        $mpdf->WriteHTML($htmlContent,);
-
-        // Gera o PDF e retorna
         return $mpdf->Output(str_replace(' ', '_', $prontuario->paciente->nome) . ' _ ' . time() . '.pdf', 'I');
     }
 }
---
+++ app/Services/PDFGeneratorService.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Setting;
+use Mpdf\Mpdf;
+
+class PDFGeneratorService
+{
+    protected $formato;
+    protected $margemEsquerda;
+    protected $margemDireita;
+    protected $margemSuperior;
+    protected $margemInferior;
+    protected $alturaCabecalho;
+    protected $alturaRodape;
+    // protected $settings;
+    protected $paciente;
+    protected $dataAtendimento;
+    protected $cabecalho;
+    protected $rodape;
+
+    public function __construct(
+        $paperSize = 'A4',
+        $layout = 'P',
+        $paciente = null,
+        $dataAtendimento = null
+    ) {
+        $this->paciente = $paciente;
+        $this->dataAtendimento = $dataAtendimento;
+
+        // Busca todas as configurações
+        $settings = Setting::getAllSettings();
+
+        $this->margemSuperior  = $settings[Setting::MARGEM_SUPERIOR];
+        $this->margemInferior  = $settings[Setting::MARGEM_INFERIOR];
+        $this->margemEsquerda  = $settings[Setting::MARGEM_ESQUERDA];
+        $this->margemDireita   = $settings[Setting::MARGEM_DIREITA];
+        $this->alturaCabecalho = $settings[Setting::ALTURA_CABECALHO];
+        $this->alturaRodape    = $settings[Setting::ALTURA_RODAPE];
+
+        $this->cabecalho = $this->replaceVariables($settings[Setting::CABECALHO]);
+        $this->rodape    = $this->replaceVariables($settings[Setting::RODAPE]);
+
+        $this->cabecalho = $this->replaceHr($this->cabecalho);
+        $this->rodape    = $this->replaceHr($this->rodape);
+
+        $this->formato = $paperSize . '-' . $layout;
+        if ($paperSize === 'A5noA4') {
+            $this->formato = 'A4-L';
+            $this->margemDireita += 148;
+        }
+    }
+
+    protected function replaceVariables($content)
+    {
+        $variaveis = [
+            '{PAC_NOME}'         => $this->paciente->nome,
+            '{NASCIM}'           => $this->paciente->nascimento->format('d/m/Y'),
+            '{IDADE}'            => $this->paciente->idade(),
+            '{SEXO}'             => $this->paciente->sexo(),
+            '{TIPO}'             => $this->paciente->tiposanguineo,
+            '{PAC_CPF}'          => $this->paciente->cpf,
+            '{PAC_CELULAR}'      => $this->paciente->celularFormatado(),
+            '{PAC_EMAIL}'        => $this->paciente->email,
+            '{PAC_CEP}'          => $this->paciente->cep,
+            '{PAC_LOGRADOURO}'   => $this->paciente->logradouro,
+            '{PAC_NUMERO}'       => $this->paciente->numero,
+            '{PAC_BAIRRO}'       => $this->paciente->bairro,
+            '{PAC_COMPLEMENTO}'  => $this->paciente->complemento,
+            '{PAC_CIDADE}'       => $this->paciente->cidade?->cidadeUf(),
+            '{DATA_ATENDIMENTO}' => $this->dataAtendimento?->format('d/m/Y'),
+        ];
+
+        return str_replace(array_keys($variaveis), array_values($variaveis), $content);
+    }
+
+    /**
+     * Substitui <hr /> por <div style="border-bottom: 2px solid #000;"></div>
+     */
+    protected function replaceHr($content)
+    {
+        return str_replace('<hr />', '<div style="border-bottom: 2px solid #000;"></div>', $content);
+    }
+
+    public function generatePDF($htmlContent)
+    {
+        // Configurando o mPDF com base nas propriedades da classe
+        $mpdf = new Mpdf([
+            'format' => $this->formato,
+            'fontDir' => [public_path('fonts/Inter')],
+            'fontdata' => [
+                'inter' => [
+                    'R' => 'Inter-VariableFont_opsz,wght.ttf',
+                    'I' => 'Inter-Italic-VariableFont_opsz,wght.ttf',
+                ],
+            ],
+            'default_font' => 'inter',
+            'margin_left' => $this->margemEsquerda,
+            'margin_right' => $this->margemDireita,
+            'margin_top' => $this->alturaCabecalho + $this->margemSuperior,
+            'margin_bottom' => $this->alturaRodape + $this->margemInferior,
+            'margin_header' => $this->margemSuperior,
+            'margin_footer' => $this->margemInferior,
+            'setAutoTopMargin' => 'stretch',
+            'setAutoBottomMargin' => 'stretch',
+            'autoMarginPadding' => -7,
+        ]);
+
+        // Define o cabeçalho e o rodapé do PDF
+        $mpdf->SetHTMLHeader('
+            <div class="document-content">
+                ' . $this->cabecalho . '
+            </div>
+        ');
+        $mpdf->SetHTMLFooter('
+            <div class="document-content">
+                ' . $this->rodape . '
+            </div>
+        ');
+
+        // Adiciona o conteúdo HTML e o CSS
+        $stylesheet = file_get_contents('vendor\ckeditor\document-content.css');
+        $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
+
+        $htmlContent = $this->replaceVariables($htmlContent);
+        $htmlContent = $this->replaceHr($htmlContent);
+
+        $mpdf->WriteHTML($htmlContent);
+
+        return $mpdf;
+    }
+}
