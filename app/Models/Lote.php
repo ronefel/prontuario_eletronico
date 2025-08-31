062fbc5397250e9fc728698a59246c4516502b6f
ronefel <ronefel@hotmail.com>
2025-08-31 12:55:54 -0400

    Ajuste nos campos de data no model Lote

--- app/Models/Lote.php
+++ app/Models/Lote.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\DatetimeWithTimezone;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Lote extends BaseModel
@@ -13,6 +14,11 @@ class Lote extends BaseModel
     protected $fillable = ['produto_id', 'numero_lote', 'data_fabricacao', 'data_validade', 'quantidade_inicial', 'quantidade_atual', 'local_id', 'status'];
 
     protected $dates = ['data_fabricacao', 'data_validade'];
+
+    protected $casts = [
+        'data_fabricacao' => DatetimeWithTimezone::class,
+        'data_validade' => DatetimeWithTimezone::class,
+    ];
 
     public function produto()
     {
